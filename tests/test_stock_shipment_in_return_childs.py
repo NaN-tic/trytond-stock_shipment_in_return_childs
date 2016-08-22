4c6fade71b6d014a9d0d3940d77ab533922288ec
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-08-22 10:50:07 +0200

    Upgrade test to version 4.0

--- tests/test_stock_shipment_in_return_childs.py
+++ tests/test_stock_shipment_in_return_childs.py
@@ -3,20 +3,14 @@
 import unittest
 import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 
 
-class TestCase(unittest.TestCase):
+class TestCase(ModuleTestCase):
     'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module(
-            'stock_shipment_in_return_childs')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'stock_shipment_in_return_childs'
 
 
 def suite():
@@ -25,5 +19,6 @@ def suite():
     suite.addTests(doctest.DocFileSuite(
             'scenario_stock_shipment_in_return_childs.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
