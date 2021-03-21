0cfa16bdcf51f372f23e02a9cd9d56e100bbc949
Albert Cervera i Areny <albert@nan-tic.com>
2021-03-21 01:03:18 +0100

    Pyflakes fixes.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,5 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from .test_stock_shipment_in_return_childs import suite
+
+__all__ = ['suite']
--- tests/test_stock_shipment_in_return_childs.py
+++ tests/test_stock_shipment_in_return_childs.py
@@ -3,9 +3,7 @@
 import unittest
 import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
-from trytond.tests.test_tryton import doctest_checker
 
 
 class TestStockShipmentInReturnChildsCase(unittest.TestCase):
